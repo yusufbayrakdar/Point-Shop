ff55bf38ab4c3c68e1a39b1c33792f6efe64039c
Yusuf Sabri Bayrakdar <32673244+yusufbayrakdar@users.noreply.github.com>
2019-05-22 08:14:41 +0300

    Update point_shop.c

--- point_shop.c
+++ point_shop.c
@@ -12,8 +12,9 @@
 typedef int bool;           /* define boolean           */
 #define true 1              /* it can be true or false  */
 #define false 0             /* it can be true or false  */
-char *read_str = NULL;      /* this array is for input.txt so it store input strings and I stored it in shared memory */
-int pointing_time;          /* this variable decide box pointing process time */
+/* I defined boxes array as static array because I need this array in more than one function because passing parameter and return the value is harder than this way */
+char *boxes = NULL;      /* this array is for child processes to read shared memory and store the read strings into this file */
+
 
 void write_shm(char *str){/* this function writes given string into shared memory */
     int len=strlen(str); /* get length of the given string */
@@ -53,13 +54,13 @@ void read_shm(){
 
     shm = shmat(shmid, NULL, 0);        /* attach shm variable to shared memory */
 
-    if(shm == (char *) -1){             /* if got an error then exit */
+    if(shm == (char *) -1){             /* if catch an error then exit */
         perror("shmat");
         exit(1);
     }
     int c=0;
-    for(s = shm; *s != 0; s++){         /* store read chars into read_str until got 0 */
-      read_str[c]=*s;
+    for(s = shm; *s != 0; s++){         /* store read chars into boxes until catch 0 */
+      boxes[c]=*s;
       c++;
    }
 }
@@ -67,28 +68,49 @@ void read_shm(){
 
 int is_there_pair(char ch,int x,int n){ /* this function takes an char, its index and total number of chars and look for is there any same char with given char if not return 0 if there is/are then return its count */ 
     int counter=0;
-    for (int i = x+1; i < n; i++)
+    for (int i = x+1; i < n; i++)       /* this loop begins to search from index of the given char to the end of the boxes array (as I mentioned above I stored input strings into this array) if there is/are any char which is the same with given char then count them and return the number */
     {
-        if(read_str[i]==ch)
+        if(boxes[i]==ch)
             counter++;
     }
     return counter;
-    
 }
 
 
+void first_write_to_output(char output_file[],int index){   /* this function is used to write changeover number into output file */
+    FILE *out = fopen(output_file, "w");            /* open output file with write operation write operation delete all text in output file and writes the given string */  
+    if (out == NULL)                                /* error check */
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    fprintf(out,"%d\n",index);   /* print process id and box color output file */
+    fclose(out);                        /* this instruction is so important, because if we don't close the file then there can be a mess and first-come-first-serve synchronization is may be broken in the same color group or we can write on the changeover number and it causes delete that line.*/
+}
+
+void write_to_output(char output_file[],int index, char color){
+    FILE *out = fopen(output_file, "a");            /* open output file with append operation, append operation doesn't delete text of output file it appends the given string */  
+    if (out == NULL)                            /* error check */
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    fprintf(out,"%d %c\n",index+100,boxes[index]);   /* print process id and box color output file */
+    fclose(out);                        /* this instruction is so important, because if we don't close the file then there can be a mess and first-come-first-serve synchronization is may be broken in the same color group. */
+}
+
 int main (int argc, char **argv){
-    int i;                          /*      loop variables          */
+    int c_index;                    /*      child index          */
     key_t shmkey;                   /*      shared memory key       */
     int shmid;                      /*      shared memory id        */
     sem_t *sem;                     /*      synch semaphore         *//*shared */
     pid_t pid;                      /*      fork pid                */
-    unsigned int n;                 /*      fork count              */
+    unsigned int box_number;        /*      fork count              */
     unsigned int value=2;           /*      semaphore value         */
-    char boxes[12];                 /*      box color list          */
     int child_id;                   /*      child process id        */
     int *priority_flag;                       /*      shared variable         *//*shared */
-    read_str = malloc(sizeof(char));
+    boxes = malloc(sizeof(char));    /*  allocate enough size for boxes array */
+    int painting_time;          /* this variable decide process time of painting box  */
 
     if (argc != 3){                 /*Check the argumants and if they are not appropriate then exit. */
         printf("Unappropriate call, please give me input and output file\n");
@@ -102,7 +124,7 @@ int main (int argc, char **argv){
         while ( fgets ( line, sizeof line, file ) != NULL )     /* read a line */
         {
             if(counter==0){                                     /* if it is first line then take tha value as box count */
-                n=(int) strtol(line, (char **)NULL, 10);
+                box_number=(int) strtol(line, (char **)NULL, 10);
             }
             else{                                               /* else then store char value in boxes array */
                 boxes[counter-1]=line[0];
@@ -131,9 +153,11 @@ int main (int argc, char **argv){
     /* initialize semaphores for shared processes */
     sem = sem_open ("pSem", O_CREAT | O_EXCL, 0644, value); 
     /* name of semaphore is "pSem", semaphore is reached using this name */
-    printf ("SEMAPHORES INITIALIZED.\n");
+    printf ("SEMAPHORE INITIALIZED.\n");
+    printf ("CRITICAL SECTION IN:->\n");
+    printf ("CRITICAL SECTION OUT:<-\n");
     /* fork child processes */
-    for (i = 0; i < n; i++){            /* create a chile until reach given box count */
+    for (c_index = 0; c_index < box_number; c_index++){            /* create a chile until reach given box count */
         pid = fork ();
         if (pid < 0) {
         /* check for error      */
@@ -143,30 +167,30 @@ int main (int argc, char **argv){
             /* if a crash occurs during the execution         */
             printf ("Fork error.\n");
         }
-        else if (pid == 0){                 /* if process is a child then get process char from the read_str */
+        else if (pid == 0){                 /* if process is a child then get process char from the boxes */
             read_shm();                     
-            switch (read_str[i])
+            switch (boxes[c_index])
                 {
                 case 'R':
-                    pointing_time=1;
+                    painting_time=1;
                     break;
                 case 'G':
-                    pointing_time=2;
+                    painting_time=2;
                     break;
                 case 'B':
-                    pointing_time=2;
+                    painting_time=2;
                     break;
                 case 'Y':
-                    pointing_time=1;
+                    painting_time=1;
                     break;
                 case 'P':
-                    pointing_time=1;
+                    painting_time=1;
                     break;
                 case 'O':
-                    pointing_time=2;
+                    painting_time=2;
                     break;
                 default:
-                    pointing_time=0;
+                    painting_time=0;
                     break;
                 }
             break;                  
@@ -178,14 +202,9 @@ int main (int argc, char **argv){
     /******************   PARENT PROCESS   ****************/
     /******************************************************/
     if (pid != 0){
-        if(i==0){ /* give child processes higher priority */
-            int which = PRIO_PROCESS;
-            int priority = -20;
-            setpriority(which, pid, priority);
-        }
         /* wait for all children to exit */
         int wait_counter=0;
-        while(wait_counter<n){
+        while(wait_counter<box_number){
             wait(NULL);
             wait_counter+=1;
         }
@@ -208,19 +227,19 @@ int main (int argc, char **argv){
     /******************   CHILD PROCESS   *****************/
     /******************************************************/
     else{
-        int x;
-        int priority[13];
-        char sorted_colors[7];
+        int priority;
+        char *sorted_colors=NULL;
+        sorted_colors = malloc(sizeof(char));
         for (int i = 0; i < 12; i++)            /* this loop section gives a sorted list with no repetition for example RRYR list -> RY */
         {
             for (int j = 0; j < 12; j++)
             {
-                if (read_str[i]==read_str[j])
+                if (boxes[i]==boxes[j])
                 {
                     for (int k = 0; k < 12; k++)
                     {
-                        if(sorted_colors[k]=='\0'||sorted_colors[k]==read_str[j]){
-                            sorted_colors[k]=read_str[j];
+                        if(sorted_colors[k]=='\0'||sorted_colors[k]==boxes[j]){
+                            sorted_colors[k]=boxes[j];
                             break;
                         }
                     }
@@ -229,30 +248,20 @@ int main (int argc, char **argv){
                 }
             }
         }
-        for (int t = 0; t < 6; t++)         /* this loop search char of child process in sorted colors and assign its index to x variable which is priority of this child */
+        for (int t = 0; t < strlen(sorted_colors); t++)         /* this loop search char of child process in sorted colors and assign its index to x variable which is priority of this child */
         {
-            if (sorted_colors[t]==read_str[i]){
-                x=t;
-                break;}
+            if (sorted_colors[t]==boxes[c_index]){
+                priority=t;
+                break;
+            }
         }
         int order=0;
-        for (int j = 0; j < i; j++)         /* this for loop gives child process its char order with in the same char for example given char is R and read chars is RYGRYYR and child pid (i) is 3 then child process knows that its second R */
+        for (int j = 0; j < c_index; j++)         /* this for loop gives child process its char order with in the same color group for example given char is R and read chars is RYGRYYR and c_index is 3 then child process knows that its second R */
         {
-            if (read_str[j]==read_str[i])
+            if (boxes[j]==boxes[c_index])
             {   
                 order+=1;
             }
-        }
-        FILE *first_out = fopen(argv[2], "w");      /* open output file with its given name with write operation */
-        FILE *out = fopen(argv[2], "a");            /* open output file with its given name with append operation */  
-        if (out == NULL)                            /* error check */
-        {
-            printf("Error opening file!\n");
-            exit(1);
-        }if (first_out == NULL)                     /* error check */
-        {
-            printf("Error opening file!\n");
-            exit(1);
         }
         bool intruder=true;                         /* this boolean variable is to unable child process to enter into critical section until critical section is completely empty for example first two process enters the critical section and one of them leaves the critical section but the other one is still in the critical section then no other child process can enter the critical section */
         while (1)                                   /* run until all child process leaves critical section */
@@ -266,22 +275,21 @@ int main (int argc, char **argv){
             /* *(priority_flag+1) variable is order counter and it checks the order of the process while processes enter the critical section.By the way let me explain order variable even if process number is 4 order can be 1 in that situation BoxList={RYGBR} process index is 4 but its second R so order is 1 (if it is first R then order is 0) */
             /* *(priority_flag+2) is a counter which counts the processes in the critical section */
             /* So this if firstly checks priority if it fits then checks order then it fits lastly checks intruder boolean so that once critical section is totaly full, no process can enter into critical section until it is fully empty */
-            if(x==*priority_flag&&*(priority_flag+1)==order&&intruder){     
+            if(priority==*priority_flag&&*(priority_flag+1)==order&&intruder){     
                 sem_wait (sem);             /* P operation */  
                 *(priority_flag+2)+=1;      /* increase critical section process counter */
-                printf("%d %c ->\n",i+100,read_str[i]);     /* show process entrance in terminal */
-                if (i==0)                                   /* if first process then print output file changeover number */
-                fprintf(first_out,"%d\n",(int)strlen(sorted_colors));    
-                usleep (pointing_time*1000);                /* wait until first order process to write into outputfile because system call takes a few miliseconds */
-                fprintf(out,"%d %c\n",i+100,read_str[i]);   /* print process id and box color output file */
+                printf("%d %c ->\n",c_index+100,boxes[c_index]);     /* show process entrance in terminal */
+                if (c_index==0)                                   /* if first process then print output file changeover number */
+                    first_write_to_output(argv[2],(int)strlen(sorted_colors));
+                write_to_output(argv[2],c_index,boxes[c_index]);     /* write the output file box index and box color */
                 *(priority_flag+1)+=1;                      /* after writing process call second order process, it means even if it is two-capacity-semaphore second order process cannot enter in critical section unless first order process calls the second one */
-                int which_char=is_there_pair(read_str[i],i,n);  /* check the process is the last char with the same priority if it is assign which_char variable to 0 */
-                usleep (pointing_time*999000);              /* pointing time */
+                int which_char=is_there_pair(boxes[c_index],c_index,box_number);  /* check the process is the last char with the same priority if it is assign which_char variable to 0 */
+                sleep (painting_time);                      /* painting time */
                 if(which_char==0){                          
                     *priority_flag+=1;                      /* if it is the last char with same priority then increase priority_flag value */
                     *(priority_flag+1)=0;                   /* if it is the last char with same priority then assign *(priority_flag+1) value to 0 */
                 }
-                printf("    <- %d %c\n",i+100,read_str[i]); /* show process leaves critical section in terminal */
+                printf("    <- %d %c\n",c_index+100,boxes[c_index]); /* show process leaves critical section in terminal */
                 *(priority_flag+2)-=1;      /* decrease critical section process counter */
                 sem_post (sem);             /* V operation */
                 break;
